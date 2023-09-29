8ff908c33e7a96af445e6a45d19ea386276719e3
변재한 <80397512+jaehanbyun@users.noreply.github.com>
2023-09-29 20:26:56 +0900

    Update update-kubespray-inventory.sh
    - Cluster name 추가하여 node 이름 반영

--- inventory/sample/update-kubespray-inventory.sh
+++ inventory/sample/update-kubespray-inventory.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Ensure we get three arguments
-if [[ $# -ne 3 ]]; then
-  echo "Usage: $0 <BASTION_IP> <MASTER_IPS> <WORKER_IPS>"
+if [[ $# -ne 4 ]]; then
+  echo "Usage: $0 <CLUSTER_NAME> <BASTION_IP> <MASTER_IPS> <WORKER_IPS>"
   exit 1
 fi
 
-BASTION_IP=$1
-MASTER_IPS=$2
-WORKER_IPS=$3
+CLUSTER_NAME=$1
+BASTION_IP=$2
+MASTER_IPS=$3
+WORKER_IPS=$4
 
 # Create inventory.ini file
 echo "[all]" > inventory.ini
@@ -18,7 +19,7 @@ count=1
 IFS=',' read -ra ADDR <<< "$MASTER_IPS"
 for ip in "${ADDR[@]}"; do
   ip_clean=$(echo $ip | tr -d ' ')
-  echo "k8s-master${count} ansible_host=${ip_clean} ip=${ip_clean} etcd_member_name=etcd${count}" >> inventory.ini
+  echo "k8s-${CLUSTER_NAME}-master${count} ansible_host=${ip_clean} ip=${ip_clean} etcd_member_name=etcd${count}" >> inventory.ini
   ((count++))
 done
 
@@ -27,18 +28,18 @@ count=1
 IFS=',' read -ra ADDR <<< "$WORKER_IPS"
 for ip in "${ADDR[@]}"; do
   ip_clean=$(echo $ip | tr -d ' ')
-  echo "k8s-worker${count} ansible_host=${ip_clean} ip=${ip_clean}" >> inventory.ini
+  echo "k8s-${CLUSTER_NAME}-worker${count} ansible_host=${ip_clean} ip=${ip_clean}" >> inventory.ini
   ((count++))
 done
 
 echo -e "\n[bastion]" >> inventory.ini
-echo "k8s-bastion ansible_host=${BASTION_IP} ansible_user=ubuntu" >> inventory.ini
+echo "k8s-${CLUSTER_NAME}-bastion ansible_host=${BASTION_IP} ansible_user=ubuntu" >> inventory.ini
 
 echo -e "\n[kube_control_plane]" >> inventory.ini
 count=1
 IFS=',' read -ra ADDR <<< "$MASTER_IPS"
 for ip in "${ADDR[@]}"; do
-  echo "k8s-master${count}" >> inventory.ini
+  echo "k8s-${CLUSTER_NAME}-master${count}" >> inventory.ini
   ((count++))
 done
 
@@ -46,7 +47,7 @@ echo -e "\n[etcd]" >> inventory.ini
 count=1
 IFS=',' read -ra ADDR <<< "$MASTER_IPS"
 for ip in "${ADDR[@]}"; do
-  echo "k8s-master${count}" >> inventory.ini
+  echo "k8s-${CLUSTER_NAME}-master${count}" >> inventory.ini
   ((count++))
 done
 
@@ -54,13 +55,13 @@ echo -e "\n[kube_node]" >> inventory.ini
 count=1
 IFS=',' read -ra ADDR <<< "$MASTER_IPS"
 for ip in "${ADDR[@]}"; do
-  echo "k8s-master${count}" >> inventory.ini
+  echo "k8s-${CLUSTER_NAME}-master${count}" >> inventory.ini
   ((count++))
 done
 count=1
 IFS=',' read -ra ADDR <<< "$WORKER_IPS"
 for ip in "${ADDR[@]}"; do
-  echo "k8s-worker${count}" >> inventory.ini
+  echo "k8s-${CLUSTER_NAME}-worker${count}" >> inventory.ini
   ((count++))
 done
 
